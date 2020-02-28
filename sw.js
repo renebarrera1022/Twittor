85360123ac63516d5434c5c3780ff6725bb993b8
Rene Barrera <rene.barrera1022@gmail.com>
2020-02-28 10:17:41 -0600

    Iniciando repositorio

---
+++ sw.js
@@ -0,0 +1,86 @@
+//imports
+importScripts('js/sw-utils.js');
+
+self.skipWaiting();
+
+const CACHE_STATIC = 'static-v1';
+const CACHE_DYNAMIC = 'dynamic-v1';
+const CACHE_INMUTABLE = 'inmutable';
+
+const appShell = [
+    '/',
+    'index.html',
+    'pages/offline.html',
+    'css/style.css',
+    'js/app.js',
+    'manifest.json',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg'
+];
+
+const appShell_Inmutable = [
+    'js/libs/jquery.js',
+    'css/animate.css',
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css'
+];
+
+function limpiarCache(cName, nItems){
+    //Limitando registros en el cache dinámico
+    caches.open(cName).then(cache=>{
+        cache.keys().then(keys=>{
+            if(keys.length > nItems){
+                cache.delete(keys[0]).then(limpiarCache(cName, nItems));
+            }
+        });
+    });
+}
+
+self.addEventListener('install', e=>{
+    //Creando cache estático e inmutable
+    const estatico = caches.open(CACHE_STATIC).then(cache=>{
+        cache.addAll(appShell);
+    });
+
+    const inmutable = caches.open(CACHE_INMUTABLE).then(cache=>{
+        cache.addAll(appShell_Inmutable);
+    });
+    e.waitUntil(Promise.all([estatico, inmutable]));
+});
+
+self.addEventListener('activate', e=>{
+    //Eliminando versiones antiguas del cache
+    const versionCache = caches.keys().then(keys=>{
+        keys.forEach(k =>{
+            if(k !== CACHE_STATIC && k.includes('static')){
+                return caches.delete(k);
+            }
+        });
+    });
+    e.waitUntil(versionCache);
+});
+
+self.addEventListener('fetch', e=>{
+    const respuesta = caches.match(e.request).then(res=>{
+        if(res) return res;
+
+        return fetch(e.request).then(newRes=>{
+            caches.open(CACHE_DYNAMIC).then(cache=>{
+                cache.put(e.request, newRes);
+                limpiarCache(CACHE_DYNAMIC, 50);
+            });
+            return newRes.clone();
+        }).catch(offline=>{
+            if(e.request.headers.get('accept').includes('text/html')){
+                return caches.match('pages/offline.html');
+            }
+        });
+    });
+    e.respondWith(respuesta);
+});
+
